2f5a4dbf38715ca619055f1c4465cb447ded530a
estherlein <esherlein@gmail.com>
2018-11-19 14:53:19 -0300

    Initial Commit.

---
+++ main.py
@@ -0,0 +1,47 @@
+import time
+import requests
+
+TICK_INTERVAL = 60  # seconds
+
+
+def main():
+    print('Starting bot...')
+
+    while True:
+        start = time.time()
+        tick()
+        end = time.time()
+
+        if end - start < TICK_INTERVAL:
+            time.sleep(TICK_INTERVAL - (end - start))
+
+
+def tick():
+    print('Running routine')
+
+    market_summaries = simple_request('https://bittrex.com/api/v1.1/public/getmarketsummaries')
+    for summary in market_summaries['result']:
+        market = summary['MarketName']
+        day_close = summary['PrevDay']
+        last = summary['Last']
+        percent_chg = ((last / day_close) - 1) * 100
+
+        if 40 < percent_chg < 60:
+            print("Fomo strikes! Let's buy some" + market + ' for ' + str(format_float(last)))
+
+        if percent_chg < -20:
+            print('We should buy some ' + market + ' for ' + str(format_float(last)))
+
+
+def simple_request(url):
+    r = requests.get(url)
+    return r.json()
+
+
+def format_float(f):
+    return "%.8f" % f
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Trade Signal Harvester
+The idea behind this is simple:
+
+####Get data
+
+####Process it 
+
+####Generate signals
